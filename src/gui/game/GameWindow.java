a0e05aa963bdb502a42544996eb06216a371951a
lkulpa <l.kulpa3@gmail.com>
2025-04-12 18:33:52 +0200

    added a simple game timer

--- src/gui/game/GameWindow.java
+++ src/gui/game/GameWindow.java
@@ -1,19 +1,24 @@
 package gui.game;
 
+import java.awt.*;
 import javax.swing.*;
 
 public class GameWindow extends JFrame {
 
     GamePanel gamePanel;
+    TimerPanel timerPanel;
 
     public GameWindow() {
         this.gamePanel = new GamePanel();
+        this.timerPanel = new TimerPanel();
 
         this.setTitle("Hanoi Tower");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLocationRelativeTo(null);
+        this.setLayout(new BorderLayout());
 
-        this.add(gamePanel);
+        this.add(timerPanel, BorderLayout.NORTH);
+        this.add(gamePanel, BorderLayout.CENTER);
 
         this.pack();
         this.setResizable(false);
---
+++ src/gui/game/TimerPanel.java
@@ -0,0 +1,36 @@
+package gui.game;
+
+import javax.swing.*;
+import java.text.DecimalFormat;
+import java.time.LocalDateTime;
+import java.awt.event.ActionEvent;
+import java.time.temporal.ChronoUnit;
+import java.awt.event.ActionListener;
+
+public class TimerPanel extends JPanel {
+
+    Timer timer;
+    JLabel clockLabel;
+    LocalDateTime startTime;
+    DecimalFormat decimalFormat = new DecimalFormat("00");
+
+    public TimerPanel() {
+        this.startTime = LocalDateTime.now();
+        this.clockLabel = new JLabel();
+        this.clockLabel.setText("00:00");
+        timer = new Timer(1000, updateClock);
+        timer.start();
+
+        this.add(clockLabel);
+    }
+
+    ActionListener updateClock = new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            int timeDeltaSeconds = (int) ChronoUnit.SECONDS.between(startTime, LocalDateTime.now());
+            int minutes = timeDeltaSeconds / 60;
+            int seconds = timeDeltaSeconds % 60;
+            clockLabel.setText(decimalFormat.format(minutes) + ":" + decimalFormat.format(seconds));
+        }
+    };
+}
